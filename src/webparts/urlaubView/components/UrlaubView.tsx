8ddfc1be3b7e2578dbbbb9a5556c5d352e0a28a8
Mathias Johan <m.johann@sms-medipool.de>
2022-03-01 14:09:01 +0100

    UrlaubView von Class component zu Functionalem Component

--- src/webparts/urlaubView/components/UrlaubView.tsx
+++ src/webparts/urlaubView/components/UrlaubView.tsx
@@ -3,23 +3,23 @@ import styles from './UrlaubView.module.scss';
 import { IUrlaubViewProps } from './IUrlaubViewProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
-export default class UrlaubView extends React.Component<IUrlaubViewProps, {}> {
-  public render(): React.ReactElement<IUrlaubViewProps> {
-    return (
-      <div className={ styles.urlaubView }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
+const UrlaubView: React.FunctionComponent<IUrlaubViewProps> = (props) => {
+  return (
+    <div className={styles.urlaubView} >
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <span className={styles.title}>Welcome to SharePoint!</span>
+            <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
+            <p className={styles.description}>{escape(props.description)}</p>
+            <a href="https://aka.ms/spfx" className={styles.button}>
+              <span className={styles.label}>Learn more</span>
+            </a>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
+
+export default UrlaubView
